e00bc288c38d0db7ac84afd46c24405e8966906b
George K. Thiruvathukal <gkt@cs.luc.edu>
2023-07-10 23:28:21 -0500

    add script for updating bibliograpy using academic

---
+++ update-bib.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+mkdir -p scratch
+pushd scratch
+wget https://github.com/gkthiruvathukal/cv/releases/latest/download/gkthiruvathukal-cv.bib
+popd
+academic import -v --bibtex scratch/gkthiruvathukal-cv.bib
+
